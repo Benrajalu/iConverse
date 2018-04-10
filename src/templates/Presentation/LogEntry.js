8c11d4d02a58878090606db65b4d344e1e24bf53
Benoit Rajalu <rajalubenoit@gmail.com>
2018-04-10 11:41:22 +0200

    Animating the LogEntry component when it's injected into the ChatLog. Its animation origin depends on whether the message is from the ChatWindow's author or not.

--- src/templates/Presentation/LogEntry.js
+++ src/templates/Presentation/LogEntry.js
@@ -7,21 +7,43 @@ import 'moment/locale/fr';
 moment.locale('fr');
 
 class LogEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {
+        opacity: 0,
+        transform: `translateX(${this.props.originalAuthor ? '50%' : '-50%'})`
+      }
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        style: {
+          opacity: 1,
+          transform: 'translateX(0%)'
+        }
+      });
+    }, 100);
+  }
   render() {
     return (
       <div
         className={
-          this.props.originalAuthor ? 'logEntry authored' : 'logEntry'
-        }>
-        <p className="author">{this.props.value.user}</p>
+          this.props.originalAuthor ? 'log_entry authored' : 'log_entry'
+        }
+        style={this.state.style}>
+        <div className="authorship">
+          <p className="author">{this.props.value.user}</p>
+          <p className="timestamp">
+            {moment(this.props.value.timestamp).calendar()}
+          </p>
+        </div>
         <div className="content">
           {this.props.value.content.map((value, index) => (
             <p key={`logEntry-${index}`}>{value}</p>
           ))}
         </div>
-        <p className="timestamp">
-          {moment(this.props.value.timestamp).calendar()}
-        </p>
       </div>
     );
   }
